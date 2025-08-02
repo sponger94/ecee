c76f73e47f4c665840bd101aa8988039b04a1b7d
javokhir <javokhir.avlikhodzhaev@gmail.com>
2025-08-02 18:30:34 +0500

    Adds full stats.c implementation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-## Readme
+# C1M1 - Statistics Analyzer
 
+## Author
+Javokir Avlikhojaev  
+Created 8/2/2025
+
+## Description
+This project is part of the Coursera course "Introduction to Embedded Systems".
--- stats.c
+++ stats.c
@@ -4,52 +4,105 @@
 //
 
 #include "stats.h"
+#include <stdio.h>
 
 void print_statistics(unsigned char* data, unsigned int size) {
-    // TODO: Implement function
+    printf("Statistics:\n");
+    printf("Minimum: %u\n", find_minimum(data, size));
+    printf("Maximum: %u\n", find_maximum(data, size));
+    printf("Mean   : %u\n", find_mean(data, size));
+    printf("Median : %u\n", find_median(data, size));
 }
 
 void print_array(unsigned char* data, unsigned int size) {
-    // TODO: Implement function
+    printf("Array Elements:\n");
+    for (unsigned int i = 0; i < size; i++) {
+        printf("%3u ", data[i]);
+        if ((i + 1) % 10 == 0)
+            printf("\n");
+    }
+    printf("\n");
 }
 
 unsigned char find_median(unsigned char* data, unsigned int size) {
-    // TODO: Implement function
-    return 0;
+    unsigned char temp[size];
+    for (unsigned int i = 0; i < size; i++) {
+        temp[i] = data[i];
+    }
+
+    // Sort temp array in descending order
+    for (unsigned int i = 0; i < size - 1; i++) {
+        for (unsigned int j = i + 1; j < size; j++) {
+            if (temp[i] < temp[j]) {
+                unsigned char t = temp[i];
+                temp[i] = temp[j];
+                temp[j] = t;
+            }
+        }
+    }
+
+    if (size % 2 == 0)
+        return (temp[size/2 - 1] + temp[size/2]) / 2;
+    else
+        return temp[size/2];
 }
 
 unsigned char find_mean(unsigned char* data, unsigned int size) {
-    // TODO: Implement function
-    return 0;
+    unsigned int sum = 0;
+    for (unsigned int i = 0; i < size; i++) {
+        sum += data[i];
+    }
+    return (unsigned char)(sum / size); // Integer division rounds down
 }
 
 unsigned char find_maximum(unsigned char* data, unsigned int size) {
-    // TODO: Implement function
-    return 0;
+    unsigned char max = data[0];
+    for (unsigned int i = 1; i < size; i++) {
+        if (data[i] > max)
+            max = data[i];
+    }
+    return max;
 }
 
 unsigned char find_minimum(unsigned char* data, unsigned int size) {
-    // TODO: Implement function
-    return 0;
+    unsigned char min = data[0];
+    for (unsigned int i = 1; i < size; i++) {
+        if (data[i] < min)
+            min = data[i];
+    }
+    return min;
 }
 
 void sort_array(unsigned char* data, unsigned int size) {
-    // TODO: Implement function
+    for (unsigned int i = 0; i < size - 1; i++) {
+        for (unsigned int j = i + 1; j < size; j++) {
+            if (data[i] < data[j]) {
+                unsigned char temp = data[i];
+                data[i] = data[j];
+                data[j] = temp;
+            }
+        }
+    }
 }
 
 int main() {
-    // Sample data
     unsigned char test[40] = {
         34, 201, 190, 154, 8, 194, 2, 6,
         114, 88, 45, 76, 123, 87, 25, 23,
         200, 122, 150, 90, 92, 87, 177, 244,
         201, 6, 12, 60, 8, 2, 5, 67,
         7, 87, 250, 230, 99, 3, 100, 90
-      };
-
+    };
     unsigned int size = 40;
 
-    // TODO: Call functions to test here
+    printf("Original Data:\n");
+    print_array(test, size);
+    print_statistics(test, size);
+
+    sort_array(test, size);
+
+    printf("\nSorted Data (Descending):\n");
+    print_array(test, size);
 
     return 0;
 }
