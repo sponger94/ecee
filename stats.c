256d689736b03f1f00aad6a5b7432a3875757a77
javokhir <javokhir.avlikhodzhaev@gmail.com>
2025-08-02 18:17:37 +0500

    Adds outline and headers for project C1M1

--- stats.c
+++ stats.c
@@ -1,3 +1,55 @@
 //
 // Created by Javokir on 8/2/2025.
+// Implementation file for statistics functions used to analyze unsigned char arrays.
 //
+
+#include "stats.h"
+
+void print_statistics(unsigned char* data, unsigned int size) {
+    // TODO: Implement function
+}
+
+void print_array(unsigned char* data, unsigned int size) {
+    // TODO: Implement function
+}
+
+unsigned char find_median(unsigned char* data, unsigned int size) {
+    // TODO: Implement function
+    return 0;
+}
+
+unsigned char find_mean(unsigned char* data, unsigned int size) {
+    // TODO: Implement function
+    return 0;
+}
+
+unsigned char find_maximum(unsigned char* data, unsigned int size) {
+    // TODO: Implement function
+    return 0;
+}
+
+unsigned char find_minimum(unsigned char* data, unsigned int size) {
+    // TODO: Implement function
+    return 0;
+}
+
+void sort_array(unsigned char* data, unsigned int size) {
+    // TODO: Implement function
+}
+
+int main() {
+    // Sample data
+    unsigned char test[40] = {
+        34, 201, 190, 154, 8, 194, 2, 6,
+        114, 88, 45, 76, 123, 87, 25, 23,
+        200, 122, 150, 90, 92, 87, 177, 244,
+        201, 6, 12, 60, 8, 2, 5, 67,
+        7, 87, 250, 230, 99, 3, 100, 90
+      };
+
+    unsigned int size = 40;
+
+    // TODO: Call functions to test here
+
+    return 0;
+}
--- stats.h
+++ stats.h
@@ -1,8 +1,71 @@
 //
 // Created by Javokir on 8/2/2025.
+// Header file for statistics utility functions.
 //
 
 #ifndef STATS_H
 #define STATS_H
 
-#endif //STATS_H
+#include <stdio.h>
+
+/**
+ * @brief Prints the statistics of an array (minimum, maximum, mean, and median).
+ *
+ * @param data Pointer to the array of unsigned char data
+ * @param size The number of elements in the array
+ */
+void print_statistics(unsigned char* data, unsigned int size);
+
+/**
+ * @brief Prints the elements of an array.
+ *
+ * @param data Pointer to the array of unsigned char data
+ * @param size The number of elements in the array
+ */
+void print_array(unsigned char* data, unsigned int size);
+
+/**
+ * @brief Finds the median value of the array.
+ *
+ * @param data Pointer to the array of unsigned char data
+ * @param size The number of elements in the array
+ * @return The median value (rounded down if needed)
+ */
+unsigned char find_median(unsigned char* data, unsigned int size);
+
+/**
+ * @brief Finds the mean (average) of the array.
+ *
+ * @param data Pointer to the array of unsigned char data
+ * @param size The number of elements in the array
+ * @return The mean value (rounded down)
+ */
+unsigned char find_mean(unsigned char* data, unsigned int size);
+
+/**
+ * @brief Finds the maximum value in the array.
+ *
+ * @param data Pointer to the array of unsigned char data
+ * @param size The number of elements in the array
+ * @return The maximum value
+ */
+unsigned char find_maximum(unsigned char* data, unsigned int size);
+
+/**
+ * @brief Finds the minimum value in the array.
+ *
+ * @param data Pointer to the array of unsigned char data
+ * @param size The number of elements in the array
+ * @return The minimum value
+ */
+unsigned char find_minimum(unsigned char* data, unsigned int size);
+
+/**
+ * @brief Sorts the array from largest to smallest.
+ *
+ * @param data Pointer to the array of unsigned char data
+ * @param size The number of elements in the array
+ */
+void sort_array(unsigned char* data, unsigned int size);
+
+#endif // STATS_H
